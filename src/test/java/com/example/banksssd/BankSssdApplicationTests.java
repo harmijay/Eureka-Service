e899294b9e584d75bc428a9c8c172b6c574b6ae4
Rizki Afrizal <srizkiafrizal85@gmail.com>
2021-09-22 16:18:00 +0700

    Initial commit

---
+++ src/test/java/com/example/banksssd/BankSssdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banksssd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSssdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
